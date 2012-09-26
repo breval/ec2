d2cf762cdf5cd86c50030431f69dafc51650b6c6
breval <billrykwalder@yahoo.com>
2012-09-26 06:27:33 -0700

    remove leading + from each line

--- user_data_script.sh
+++ user_data_script.sh
@@ -1,18 +1,18 @@
-+#!/bin/sh
-+set -e -x
-+
-+apt-get --yes --quiet update
-+apt-get --yes --quiet install git puppet-common
-+
-+#
-+# Fetch puppet configuration from public github repository.
-+#
-+
-+mv /etc/puppet /etc/puppet.orig
-+git clone https://github.com/breval/ec2.git /etc/puppet
-+
-+#
-+# Run puppet.
-+#
-+
-+puppet apply /etc/puppet/manifests/init.pp
+#!/bin/sh
+set -e -x
+
+apt-get --yes --quiet update
+apt-get --yes --quiet install git puppet-common
+
+#
+# Fetch puppet configuration from public github repository.
+#
+
+mv /etc/puppet /etc/puppet.orig
+git clone https://github.com/breval/ec2.git /etc/puppet
+
+#
+# Run puppet.
+#
+
+puppet apply /etc/puppet/manifests/init.pp
