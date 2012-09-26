25837ce8824f98c570c598530e954b9982ec0031
breval <billrykwalder@yahoo.com>
2012-09-26 16:55:07 -0700

    Comment-out require lines for testing

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -6,24 +6,24 @@ class apache2 {
     service { 'apache2':
         ensure => running,
         enable => true,
-        require => Package["apache2"],
+#        require => Package["apache2"],
 		}
 		    
     		file { "/var/www/eval/":
 			ensure => directory,
-  			require => Package['apache2']	
+#  			require => Package['apache2']	
 		}
 
 		file { "/var/www/eval/index.html":
          		ensure => present,
          		source => "/etc/puppet/private/www/index.html",
-         		require => Package['apache2']
+#         		require => Package['apache2']
                 }
 
                 file { "/etc/apache2/sites-available/eval":
 			ensure => present,
                         source => "/etc/puppet/private/www/eval",
-                        require => Package['apache2']
+#                        require => Package['apache2']
                 }
 
 		exec { "enable virtual site - apache":
