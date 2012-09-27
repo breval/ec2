cd86a3e4a5cc022129a98a0f8dfb61d67c76111c
breval <billrykwalder@yahoo.com>
2012-09-26 17:05:28 -0700

    clean up file - move apache refresh stuff to main shell script

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -6,33 +6,19 @@ class apache2 {
     service { 'apache2':
         ensure => running,
         enable => true,
-#        require => Package["apache2"],
 		}
 		    
     		file { "/var/www/eval/":
 			ensure => directory,
-#  			require => Package['apache2']	
 		}
 
 		file { "/var/www/eval/index.html":
          		ensure => present,
          		source => "/etc/puppet/private/www/index.html",
-#         		require => Package['apache2']
                 }
 
                 file { "/etc/apache2/sites-available/eval":
 			ensure => present,
                         source => "/etc/puppet/private/www/eval",
-#                        require => Package['apache2']
                 }
-
-		exec { "enable virtual site - apache":
-    			command => "a2ensite eval",
-    			path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-		}
-
-		exec { "bounce apache":
-    			command => "service apache2 reload",
-    			path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-			}
 }
