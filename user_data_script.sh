a2e3370bf5b627a56001df67913862fd92bcff84
breval <billrykwalder@yahoo.com>
2012-09-26 17:09:45 -0700

    include virtual site and bounce

--- user_data_script.sh
+++ user_data_script.sh
@@ -12,7 +12,13 @@ mv /etc/puppet /etc/puppet.orig
 git clone https://github.com/breval/ec2.git /etc/puppet
 
 #
-# Run puppet.
+# Run puppet, build site.
 #
 
 puppet apply /etc/puppet/manifests/init.pp
+
+#
+# Load eval virtual site and restart service - keep it simple
+#
+a2ensite eval
+service apache2 reload
